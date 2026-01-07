f203eb5b89e15dc0a512c038a284b6ac2b6e2069
dylan dantonio <dylan.dantonio@ASTIDOM.com>
2026-01-07 16:42:40 +0100

    test(e2e): ajout de tests pour les services Login, Register, Teacher

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Login spec', () => {
   it('Login successfull', () => {
-    cy.visit('/login')
+    cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -10,20 +10,41 @@ describe('Login spec', () => {
         username: 'userName',
         firstName: 'firstName',
         lastName: 'lastName',
-        admin: true
+        admin: true,
       },
-    })
+    });
 
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []
+    ).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
+
+  it('Do nothing if the form is invali', () => {
+    cy.visit('/login');
+    cy.get('button[type=submit]').should('be.disabled');
+    cy.url().should('include', '/login');
+  });
+
+it('Display an error if the login fail', () => {
+  cy.visit('/login');
+  cy.intercept('POST', '/api/auth/login', { statusCode: 401 }).as('loginFail');
+
+  cy.get('input[formControlName=email]').type('bad@user.com');
+  cy.get('input[formControlName=password]').type('wrongpass');
+  cy.get('button[type=submit]').should('not.be.disabled').click();
+
+  cy.wait('@loginFail');
+  cy.contains('An error occurred').should('be.visible');
+});
 });
